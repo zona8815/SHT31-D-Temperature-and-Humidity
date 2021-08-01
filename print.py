67e90f5a06f8a58895eeec69bdc5de60f8d694ef
zona8815 <emmachu68@gmail.com>
2021-08-01 18:14:44 +0800

    add watering func

--- print.py
+++ print.py
@@ -11,14 +11,35 @@ class TemperatureHumidity:
         return
 
     def getTemperatureData(self):
-        print('Temp = {:.1f} \u00b0C'.format(sensorSHT31.temperature))
-        return
+        #print('Temp = {:.1f} \u00b0C'.format(sensorSHT31.temperature))
+        TData = format(sensorSHT31.temperature, '.3f')
+        return TData
 
     def getHumidityData(self):
-        print('RH = {:.1f} %\n'.format(sensorSHT31.relative_humidity))
-        return
+        #print('RH = {:.1f} %\n'.format(sensorSHT31.relative_humidity))
+        RHData = format(sensorSHT31.relative_humidity, '.3f')
+        return RHData
 
 TandH = TemperatureHumidity()
+RH = sensorSHT31.relative_humidity
 
-TandH.getTemperatureData()
-TandH.getHumidityData()
+class Watering:
+    def __init__(self):
+        return
+    
+    def getWateringData(self):
+        if RH < 50.0:
+            answer = "Need water. "
+        elif RH > 60.0:
+            answer = "Too much water. "
+        else:
+            answer = "It's fine now. "
+        return answer
+
+Water = Watering()
+
+#TandH.getTemperatureData()
+#TandH.getHumidityData()
+
+print("溫度: " + str(TandH.getTemperatureData()) + "<br/>" + "濕度: " + str(TandH.getHumidityData()))
+print("澆水: " + str(Water.getWateringData()))
