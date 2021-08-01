dda5ceaa03b6f2c86ab35ca8d72e0e9131b1cdda
zona8815 <emmachu68@gmail.com>
2021-08-01 17:09:37 +0800

    Class test for SHT31 and print

--- print.py
+++ print.py
@@ -4,18 +4,18 @@ import adafruit_sht31d
 import time
 
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_sht31d.SHT31D(i2c, 0x44)
+sensorSHT31 = adafruit_sht31d.SHT31D(i2c, 0x44)
 
 class TemperatureHumidity:
     def __init__(self):
         return
 
     def getTemperatureData(self):
-        print('Temp = {:.1f} \u00b0C'.format(sensor.temperature))
+        print('Temp = {:.1f} \u00b0C'.format(sensorSHT31.temperature))
         return
 
     def getHumidityData(self):
-        print('RH = {:.1f} %\n'.format(sensor.relative_humidity))
+        print('RH = {:.1f} %\n'.format(sensorSHT31.relative_humidity))
         return
 
 TandH = TemperatureHumidity()
